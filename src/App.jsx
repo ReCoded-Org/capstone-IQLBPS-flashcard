38f84bf3e3dbaf79293ef4ea271a025ac04f6a01
sara khalil <sarakhalil3377@gmail.com>
2022-10-26 20:53:13 +0300

    setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Routes, Route, Outlet } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Team from "./Pages/Team";
+import Profile from "./Pages/Profile";
+import NoMatch from "./Pages/NoMatch";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Routes>
+    <Route path="/"  element={<Layout />}>
+    <Route index element={<Home/>}/>
+    <Route path="about" element={<About/>}/>
+    <Route path="contact" element={<Contact/>}/>
+    <Route path="team" element={<Team/>}/>
+    <Route path="profile" element={<Profile/>}/>
+    <Route path="*" element={<NoMatch/>}/>
+
+    </Route>
+    </Routes>
+    
   );
 }
+function Layout(){
+  return(
+<div>
 
+<Outlet/>
+</div>
+
+  )
+}
 export default App;
