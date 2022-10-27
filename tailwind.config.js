d123ce93e21550a05fb2ebb9cacac7b561d27b67
Muhanned Noman <Muhanned.Noman@gmail.com>
2022-10-27 20:42:33 +0300

    Style: reconfigure tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { fontFamily as _fontFamily } from 'tailwindcss/defaultTheme';
-
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-extraneous-dependencies
+const defaultTheme = require('tailwindcss/defaultTheme');
 
-export const content = ['./src/**/*.{js,jsx,ts,tsx}'];
-export const theme = {
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
-  extend: {
-    colors: {
-      primary: {
-        50: '#EDF6FD',
-        100: '#DFEFFB',
-        200: '#BBDDF7',
-        300: '#9BCDF3',
-        400: '#7BBDEF',
-        500: '#58ABEB',
-        600: '#208FE4',
-        700: '#156BAD',
-        800: '#0E4672',
-        900: '#07253B',
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#EDF6FD',
+          100: '#DFEFFB',
+          200: '#BBDDF7',
+          300: '#9BCDF3',
+          400: '#7BBDEF',
+          500: '#58ABEB',
+          600: '#208FE4',
+          700: '#156BAD',
+          800: '#0E4672',
+          900: '#07253B',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
     },
-    fontFamily: {
-      sans: ['Inter', ..._fontFamily.sans],
-    },
   },
+  plugins: [],
 };
-export const plugins = [];
-
