e63b0cc88c5ca9603bcdc2c6460c96c310f096b8
rch20 <reemshalak22@hotmail.com>
2022-10-27 20:14:21 +0300

    Added to config : 1.Added a color pallete in tailwind.config.js 2. Added Inter as a default font in tailwind.config.js 3. Imported the font in index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { fontFamily as _fontFamily } from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
+
+export const content = ['./src/**/*.{js,jsx,ts,tsx}'];
+export const theme = {
+  darkMode: 'class',
+  extend: {
+    colors: {
+      primary: {
+        50: '#EDF6FD',
+        100: '#DFEFFB',
+        200: '#BBDDF7',
+        300: '#9BCDF3',
+        400: '#7BBDEF',
+        500: '#58ABEB',
+        600: '#208FE4',
+        700: '#156BAD',
+        800: '#0E4672',
+        900: '#07253B',
+      },
+    },
+    fontFamily: {
+      sans: ['Inter', ..._fontFamily.sans],
+    },
   },
-  plugins: [],
 };
+export const plugins = [];
