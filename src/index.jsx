4a061b7bd0291150e01566a4a0ce8294176bd936
omarbanat <o.j.banat@gmail.com>
2022-10-27 19:51:40 +0300

    feat: add Suspense to app.js - continues to i18next config

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import './index.css';
+import i18n from './i18n';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -11,7 +12,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Suspense fallback="loading...">
+        <App />
+      </Suspense>
     </Provider>
   </React.StrictMode>
 );
