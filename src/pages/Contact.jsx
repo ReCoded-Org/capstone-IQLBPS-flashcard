827a73a34eadf5dbfa193140dcef69723ddf7a7b
Nasam Sayour <nasamsay@gmail.com>
2022-10-28 14:19:00 +0300

    Feat: Contact us page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Contact = () => {
-  return <div>Contact</div>;
-};
+  const {register, formState: {errors}, handleSubmit} = useForm();
+  const onSubmit = data => console.log(data)
+  return (
+    <section className="bg-white dark:bg-gray-900" onSubmit={handleSubmit(onSubmit)}>
+      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
+          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+          <form action="#" className="space-y-8">
+              <div>
+                  <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your name
+                  <input type="text" id="name" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name" name='name' {...register("name", {required: "Please enter you name",})} />
+                  {errors.name && <div>{errors.name.message}</div>}
+                  </label>
+              </div>
+              <div>
+                  <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email
+                  <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@gmail.com" name='email' {...register("email", {required:"Please enter your email",})} />
+                  {errors.email && <div>{errors.email.message}</div>}
+                  </label>
+              </div>
+              <div className="sm:col-span-2">
+                  <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message
+                  <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a message..." name='message' {...register("message", {required: "Please enter your message",})} />
+                  {errors.message && <div>{errors.message.message}</div>}
+                  </label>
+              </div>
+              <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+          </form>
+      </div>
+  </section>
+  )};
 
 export default Contact;
