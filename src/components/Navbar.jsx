edc63260e74ce25c7b3f4841260d7565453a7968
rch20 <reemshalak22@hotmail.com>
2022-10-29 20:53:09 +0300

    Style : Styling Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import { Navbar , Button  , DarkThemeToggle ,Dropdown , Avatar} from 'flowbite-react'
+
+
+const Nav = () => {
+  return (
+<Navbar fluid>
+  <Navbar.Brand href="https://flowbite.com/">
+    <img
+      src="https://flowbite.com/docs/images/logo.svg"
+      className="mr-3 h-6 sm:h-9"
+      alt="Flowbite Logo"
+    />
+    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+      Flowbite
+    </span>
+  </Navbar.Brand>
+  <div className="flex md:order-2">
+    <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1" >
+      <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" /></svg>
+      <span className="sr-only">Search</span>
+    </button>
+    <div className="hidden relative md:block">
+      <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+        <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" /></svg>
+        <span className="sr-only">Search icon</span>
+      </div>
+      <input type="text" id="search-navbar" className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
+    </div>
+    <button data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
+      <span className="sr-only">Open menu</span>
+      <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
+    </button>
+  </div>
+  <div className="flex space-x-4 md:order-2">
+    <Button>
+      Log in
+    </Button>
+  
+    <Button>
+    Sign up
+    </Button>
+    <Navbar.Toggle />
+    <DarkThemeToggle />
+  </div>
+ 
+
+  <div className="flex md:order-2">
+    <Dropdown
+      arrowIcon={false}
+      inline
+      label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded/>}
+    >
+      <Dropdown.Header>
+        <span className="block text-sm">
+          Bonnie Green
+        </span>
+        <span className="block truncate text-sm font-medium">
+          name@flowbite.com
+        </span>
+      </Dropdown.Header>
+      <Dropdown.Item>
+        Dashboard
+      </Dropdown.Item>
+      <Dropdown.Item>
+        Settings
+      </Dropdown.Item>
+      <Dropdown.Item>
+        Earnings
+      </Dropdown.Item>
+      <Dropdown.Divider />
+      <Dropdown.Item>
+        Sign out
+      </Dropdown.Item>
+    </Dropdown>
+    <Navbar.Toggle />
+  </div>
+  <Navbar.Collapse>
+    <Navbar.Link
+      href="/navbars"
+      active
+    >
+      Home
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      About
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      Services
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      Pricing
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      Contact
+    </Navbar.Link>
+  </Navbar.Collapse>
+</Navbar>
+  )
+}
+
+export default Nav
