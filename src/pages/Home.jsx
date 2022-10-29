655333734b0a38e6344a5006d63d4128b52363b5
Hayat Salawdeh <35167530+hayatzsala@users.noreply.github.com>
2022-10-29 18:53:17 +0200

    feat: design feature sections (#63)
    * feat: design feature sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import FeaturePoints from '../components/feature/FeaturePoints';
+import FeatureCard from '../components/feature/FeatureCard';
+import {
+  LEARNING_FEATURES,
+  WEBSITE_FEATURES,
+} from '../assets/feature/Features';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      {LEARNING_FEATURES.map((feature) => (
+        <FeatureCard feature={feature} key={feature.name} />
+      ))}
+      <FeaturePoints features={WEBSITE_FEATURES} />
+    </div>
+  );
 };
 
 export default Home;
