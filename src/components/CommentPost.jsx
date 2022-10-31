f73367460c4b691c0c34cb19c6f53e8dffeca3c0
Muhanned Noman <Muhanned.Noman@gmail.com>
2022-10-31 18:26:30 +0300

    Fix: eslint label error

--- src/components/CommentPost.jsx
+++ src/components/CommentPost.jsx
@@ -1,22 +1,24 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-
-// !!! REMOVE THE ABOVE COMMENT IF YOU PLAN TO CHANGE THIS FILE !!!
-
-export default function CommentPost(){
-
-
-    return(
-        <form className="mb-6">
-        <div className="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-            <label htmlFor="comment" className="sr-only"> Your comment </label>
-            <textarea id="comment" rows="6" 
+export default function CommentPost() {
+  return (
+    <form className="mb-6">
+      <div className="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+        <label htmlFor="comment" className="sr-only">
+          Your comment
+          <textarea
+            id="comment"
+            rows="6"
             className="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
-            placeholder="Write a comment..." required/> 
-        </div>
-        <button type="submit"
-            className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-            Post comment
-        </button>
+            placeholder="Write a comment..."
+            required
+          />
+        </label>
+      </div>
+      <button
+        type="submit"
+        className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
+      >
+        Post comment
+      </button>
     </form>
-    )
+  );
 }
