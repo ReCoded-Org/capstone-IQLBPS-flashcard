931cb8f8c75e41e6d6e0cb0bfea1944cdd1168d1
Muhanned Noman <Muhanned.Noman@gmail.com>
2022-11-01 19:53:37 +0300

    Fix: design of login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,35 +1,74 @@
-import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
-export default function Login(){
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+import GoogleSignInButton from "../components/GoogleSignInButton";
 
-  function handleFormSubmit(e){
-    e.preventDefault();
-  }
-    return(
-      <div className="grid h-screen place-items-center">
-      <div className="xl:w-1/3 lg:w-2/5 md:w-3/5 sm:1/2 w-full">
-        <form onSubmit={handleFormSubmit}>
-        <h1 className="text-2xl font-medium pb-8">Log in to Your Account</h1>
-        <div className="mb-6 m-8 justify-center items-center m-auto">
-          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 m-auto">Your email
-          <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-96 m-auto" placeholder="Email" required="" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-          </label>
-          </div>
-        <div className="mb-6 m-8 justify-center items-center m-auto">
-          <label htmlFor='password'className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password
-          <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-96 m-auto" placeholder="Password" required="" value={password} onChange={(e)=>setPassword(e.target.value)}/>
-        </label>
-          </div>
-        <button type="submit" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-48 m-auto">Log in</button>
-      </form>
-      <a className="text-black text-opacity-40 hover:underline mt-2 mb-4 w-full sm:mb-3 flex flex-col space-y-4" href="/register"> Don`t Have an Account ? Sign up</a>
-      <button type="button" className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
-  <svg className="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"/><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"/>
-  Sign in with Google
-</button>
-      </div>
-      </div>
-    );
+const loginSchema = yup.object().shape({
+  email: yup.string().email('Must be a valid email address').required('Email is required'),
+  password: yup.string().required("Password is required"),
+});
+
+const Login = () => {
+  const {t} = useTranslation();
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(loginSchema)
+  });
+
+  const onSubmit = async (data) => {
+    console.log(data);
+  };
+
+  return(
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <Link to="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+            Flashcards    
+        </Link>
+    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                {t('Sign in to your account')}
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                  <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email
+                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" {...register('email')} />
+                    {errors.email && (
+                      <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                        <span className=" font-medium">
+                          {t(`${errors.email?.message}`)}
+                        </span>
+                      </p>
+                    )}
+                    </label>
+                </div>
+                <div>
+                  <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password
+                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" {...register('password')}/>
+                    {errors.password && (
+                      <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                        <span className=" font-medium">
+                          {t(`${errors.password?.message}`)}
+                        </span>
+                      </p>
+                    )}
+                    </label>
+                </div>
+                <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                <GoogleSignInButton />
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                    {t('Dont have an account yet?')} <Link to="/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
+                </p>
+            </form>
+        </div>
+    </div>
+</div>
+</section>
+  );
 }
+
+export default Login
