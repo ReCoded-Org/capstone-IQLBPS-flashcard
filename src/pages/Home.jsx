927ee45992f9aa4db5047ecb9be14c0029b453e0
Nijyar Ghazi <nijyar360@gmail.com>
2022-11-02 23:12:48 +0300

    Public profle - undid changes to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,25 @@
 import React from 'react';
 
-// import FeaturePoints from '../components/feature/FeaturePoints';
-// import FeatureCard from '../components/feature/FeatureCard';
-// import {
-//   LEARNING_FEATURES,
-//   WEBSITE_FEATURES,
-// } from '../assets/feature/Features';
+import FeaturePoints from '../components/feature/FeaturePoints';
+import FeatureCard from '../components/feature/FeatureCard';
+import {
+  LEARNING_FEATURES,
+  WEBSITE_FEATURES,
+} from '../assets/feature/Features';
 
-// import Hero from "../components/Hero"
+import Hero from "../components/Hero"
 
-
-import PublicProfile from '../components/PublicProfile'
 
 const Home = () => {
-  // return (
-  //   <div>
-  //   <Hero/>
-  //     {LEARNING_FEATURES.map((feature) => (
-  //       <FeatureCard feature={feature} key={feature.name} />
-  //     ))}
-  //     <FeaturePoints features={WEBSITE_FEATURES} />
-  //   </div>
-  // );
-  return(
-    <PublicProfile/>
-  )
+  return (
+    <div>
+    <Hero/>
+      {LEARNING_FEATURES.map((feature) => (
+        <FeatureCard feature={feature} key={feature.name} />
+      ))}
+      <FeaturePoints features={WEBSITE_FEATURES} />
+    </div>
+  );
 };
 
 export default Home;
