a5d120e9339724e9a2972269303ae280a57b189c
Muhanned Noman <Muhanned.Noman@gmail.com>
2022-11-05 20:17:01 +0300

    Fix: contact page

--- src/services/contact.js
+++ src/services/contact.js
@@ -1,34 +1,18 @@
-import React, { useRef } from 'react';
 import emailjs from 'emailjs-com';
-
 
 const SERVICE_ID = 'service_8owfnsk';
 const TEMPLATE_ID = 'template_5xezz2u';
 const USER_ID = 'CYdQVZe5Ev-tZ83_A';
-export const ContactUs = () => {
-  const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+const sendEmail = (formElement) => {
+  emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, formElement, USER_ID).then(
+    (result) => {
+      console.log('Success', result);
+    },
+    (error) => {
+      console.log('error', error);
+    }
+  );
+};
 
-const sendEmail= (text) => {
-    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, text, USER_ID)
-      .then((result) => {
-        //TODO: create alert if its successful
-      }, (error) => {
-        //TODO: create alert if something went wrong
-      });
-      return (
-        <form ref={form} onSubmit={sendEmail}>
-          <label>Name</label>
-          <input type="text" name="user_name" />
-          <label>Email</label>
-          <input type="email" name="user_email" />
-          <label>Message</label>
-          <textarea name="message" />
-          <input type="submit" value="Send" />
-        </form>
-      );
-    };
-  }
 export default sendEmail;
