6544baaf1ce74fa7a9f4351050683b81dee45fa8
Muhanned Noman <Muhanned.Noman@gmail.com>
2022-11-07 19:16:31 +0300

    Fix: home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 
 import FeaturePoints from '../components/feature/FeaturePoints';
 import FeatureCard from '../components/feature/FeatureCard';
-import About from './About';
+import Hero from '../components/Hero';
 
 import {
   LEARNING_FEATURES,
   WEBSITE_FEATURES,
 } from '../assets/feature/Features';
 
-
 const Home = () => {
   return (
     <div>
-    <About/>
+      <Hero />
       {LEARNING_FEATURES.map((feature) => (
         <FeatureCard feature={feature} key={feature.name} />
       ))}
