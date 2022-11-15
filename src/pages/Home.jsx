316cdf49c4bc119ebf1de487c24c6b72ac5a445c
Muhanned Noman <Muhanned.Noman@gmail.com>
2022-11-15 17:24:27 +0300

    Fix: remove duple import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,10 @@ import React from 'react';
 import FeaturePoints from '../components/feature/FeaturePoints';
 import FeatureCard from '../components/feature/FeatureCard';
 import Hero from '../components/Hero';
-
 import {
   LEARNING_FEATURES,
   WEBSITE_FEATURES,
 } from '../assets/feature/Features';
-
-import Hero from '../components/Hero';
 
 const Home = () => {
   return (
