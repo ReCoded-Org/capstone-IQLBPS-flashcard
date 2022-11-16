616f77910c6ef90ccedb0c826398b27109133c43
Reem Chalak <62399527+rch20@users.noreply.github.com>
2022-11-16 20:33:55 +0200

    Functionality :  Added Latest/Favorited Sets in the UserHomePage (#112)
    * Functionality :  Added Latest/Favorited Sets in the UserHomePage

--- src/pages/UserHome.jsx
+++ src/pages/UserHome.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
 import { ActiveUserSection } from '../components/user/ActiveUserSection';
-import { lastsList, ratedList, suggestionList } from '../util/fakeData';
+import { getFavoriteSets, latestAddedSets } from '../services/user';
+import Loading from '../components/Loading';
 
 const UserHome = () => {
+  const [latestSets, setLatestSets] = useState([]);
+  const [isLoadingLatest, setIsLoadingLatest] = useState(true);
+
+  const [favoriteSets, setFavoriteSets] = useState([]);
+  const [isLoadingFav, setIsLoadingFav] = useState(true);
+
+  const { user } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const getLatestSets = async () => {
+      const sets = await latestAddedSets();
+      setLatestSets(sets);
+      setIsLoadingLatest(false);
+    };
+    getLatestSets();
+  }, []);
+
+  useEffect(() => {
+    const getFavSets = async () => {
+      const favSets = await getFavoriteSets(user.uid);
+      setFavoriteSets(favSets);
+      setIsLoadingFav(false);
+    };
+    getFavSets();
+  }, [user]);
+
   return (
-    <section className="bg-white dark:bg-gray-900  flex flex-col justify-center items-center overflow-autos p-4 pb-20">
-      <ActiveUserSection list={ratedList} title="Rated by you" />
-      <ActiveUserSection list={suggestionList} title="Suggestion" />
-      <ActiveUserSection list={lastsList} title="Lasts" />
+    <section className="bg-white dark:bg-gray-900  flex flex-col  overflow-autos p-4 pb-20">
+      {isLoadingFav ? (
+        <Loading />
+      ) : (
+        <ActiveUserSection list={favoriteSets} title="Your Favorites" />
+      )}
+      {isLoadingLatest ? (
+        <Loading />
+      ) : (
+        <ActiveUserSection list={latestSets} title="Latest Sets" />
+      )}
     </section>
   );
 };
