ae67d1bb8f3761e7587d89f1e6422abbad89c93f
Lelian248 <53427379+Lelian248@users.noreply.github.com>
2022-11-18 03:00:48 +0200

    Feature: Protecte routes to be accessible after being loged in.

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import {Outlet, Navigate} from 'react-router-dom';
+import {AuthContext}from '../context/FirebaseAuthContext';
+
+
+
+export default function ProtectedRoute(){
+   
+    const authValue=useContext(AuthContext)
+    console.log(authValue)
+
+    if (authValue.userDataPresent){
+        if(authValue.user == null){
+            return (
+                <Navigate to='/login'/>
+            )
+        }return(<Outlet/>)
+    }return null
+}
