7ba58b06850477640e2cf08bf1c353b0cfb63d31
Lelian248 <53427379+Lelian248@users.noreply.github.com>
2022-11-19 18:01:26 +0200

    Feature: Protecte routes to be accessible after being loged in. (#116)
    * Feature: Protecte routes to be accessible after being loged in.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route, Outlet } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useDispatch, useSelector } from 'react-redux';
+import { auth } from './services/firebaseConfig';
+import { login, logout, selectUser } from './features/user/userSlice';
 
 import Home from './pages/Home';
 import About from './pages/About';
@@ -18,8 +20,8 @@ import UserHome from './pages/UserHome';
 import Footer from './components/Footer';
 import Nav from './components/Navbar';
 import PublicProfile from './pages/PublicProfile';
-import { login, logout, selectUser } from './features/user/userSlice';
-import { auth } from './services/firebaseConfig';
+import ProtectedRoute from './components/ProtectedRoute'
+import FirebaseAuthContext from './context/FirebaseAuthContext';
 
 const App = () => {
   const user = useSelector(selectUser);
@@ -44,6 +46,7 @@ const App = () => {
   }, []);
 
   return (
+    <FirebaseAuthContext>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={user ? <UserHome /> : <Home />} />
@@ -52,13 +55,18 @@ const App = () => {
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
         <Route path="team" element={<Team />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="library" element={<Library />} />
+
+        <Route element={<ProtectedRoute />}>
+            <Route path="profile" element={<Profile />} exact/>
+            <Route path="library" element={<Library />} exact/>
+        </Route>
+        
         <Route path="review" element={<Review />} />
         <Route path="user/:id" element={<PublicProfile />} />
         <Route path="*" element={<NoMatch />} />
       </Route>
     </Routes>
+    </FirebaseAuthContext>
   );
 };
 
@@ -68,7 +76,7 @@ const Layout = () => {
 
   return (
     <div>
-      <Nav />
+       <Nav />
       <Outlet />
       <Footer />
     </div>
