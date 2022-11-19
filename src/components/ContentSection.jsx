d87c77491d613a57b37c7253851dc516a2e6046e
Muhanned Noman <Muhanned.Noman@gmail.com>
2022-11-19 18:48:31 +0300

    Refactor: use existing components

--- src/components/ContentSection.jsx
+++ src/components/ContentSection.jsx
@@ -1,95 +1,31 @@
-/* eslint-disable jsx-a11y/control-has-associated-label */
-/* eslint-disable react/button-has-type */
-import React, { useState } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { v4 as uuidv4 } from 'uuid';
 
+import SetCarousel from './SetCarousel';
+import SetCard from './SetCard';
 
 function ContentSection() {
+  const data = [
+    {
+      front: 'FirstSlider',
+      back: 'my sound is waf waf !',
+    },
+    {
+      front: 'SecondSlider',
+      back: 'I round and red',
+    },
+    {
+      front: 'thirdSlider',
+      back: 'Square color brown and use to take lunch ',
+    },
+  ];
 
-   const data = [
-
-      {
-       
-         id: 1,
-         image: "https://upload.wikimedia.org/wikipedia/commons/1/18/Dog_Breeds.jpg",
-         title: 'FirstSlider',
-         question:'my sound is waf waf !'
-      },
-      {
-         id: 2,
-         image: "https://images.unsplash.com/photo-1570913149827-d2ac84ab3f9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80",
-         title: 'SecondSlider',
-         question:'I round and red'
-      },
-      {
-         id: 3,
-         image: "https://5.imimg.com/data5/NN/GS/MY-41367701/wooden-table-500x500.jpg",
-         title: 'thirdSlider',
-         question:'Square color brown and use to take lunch '
-      }
-   ];
-
-   const [activeSlide, setActiveSlide] = useState(1);
-
-
-   const prevSliderHandler = (id) => {
-      if (id === 1) {
-         setActiveSlide(data.length)
-      } else if (id > 1) {
-         setActiveSlide(activeSlide - 1)
-      } else {
-         setActiveSlide(data.length - 1);
-      }
-   }
-
-   const nextSliderHandler = (id) => {
-      if (id === data.length) {
-         setActiveSlide(1)
-      } else if (id < data.length) {
-         setActiveSlide(activeSlide + 1)
-      } else {
-         setActiveSlide(data.length - 1);
-      }
-   }
-
-   return (
-      <div className='m-6'>
-         {data.map((item) => {
-            const { id, image, title ,question} = item;
-            return (
-               <div key={id} className={
-                  activeSlide === id
-                     ? 'flex justify-between items-center'
-                     : 'hidden'}
-               >
-
-
-
-                  <button type="button" className="w-3 h-3 rounded-full  test-6xl border-2 border-black" aria-current="false" aria-label="Slide 1" onClick={() =>
-                     prevSliderHandler(id)}>
-                     <FiChevronRight />
-                  </button>
-                  <div className='flex flex-col items-center'>
-                     <p>{question}</p>
-                     <img src={image} alt={title} />
-                     <h2 className='text-2xl font-bold my-6'>{id}</h2>
-                     <h3 className='text-sl'>{title}</h3>
-                  </div>
-                  <button type="button" className="w-3 h-3 rounded-full  test-6xl border-2 border-black" aria-current="false" aria-label="Slide 1" onClick={() =>
-                     nextSliderHandler(id)}>
-                     <FiChevronLeft />
-                  </button >
-               </div>
-            );
-         })}
-      </div>
-   )
-};
-
-
-
-
-
-
+  return (
+    <SetCarousel>
+      {data.map((item) => (
+        <SetCard key={uuidv4()} front={item.title} back={item.back} />
+      ))}
+    </SetCarousel>
+  );
+}
 
 export default ContentSection;
--- src/components/SetCard.jsx
+++ src/components/SetCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const SetCard = ({ text }) => {
+const SetCard = ({ front, back }) => {
   return (
     <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-      {text}
+      {front} {back}
     </div>
   );
 };
