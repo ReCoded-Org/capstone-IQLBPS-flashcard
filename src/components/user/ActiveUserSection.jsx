ef250620c715739090f3f2aad08e165558e34659
rch20 <reemshalak22@hotmail.com>
2022-11-24 21:34:24 +0200

    Bug Functionality : Favorite Sets only appear if there are any.

--- src/components/user/ActiveUserSection.jsx
+++ src/components/user/ActiveUserSection.jsx
@@ -5,8 +5,8 @@ import Card from '../Card';
 export const ActiveUserSection = ({ list, title }) => {
   return (
     <div className="mb-8 rounded-md">
-      <p className="text-lg text-black font-bold my-6">
-        {'>  '} {title}
+      <p className="text-lg text-black font-bold my-6 dark:text-white">
+         {title}
       </p>
       <div className="h-96 md:h-full sm:h-full text-black flex flex-row flex-wrap  items-start justify-start gap-8">
         {list.map((set) => (
