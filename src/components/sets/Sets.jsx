d850a30c23394fcc60e1a83a2ceeacae22d8756b
Omar Banat <49315137+omarbanat@users.noreply.github.com>
2022-11-24 21:53:19 +0200

    fix: issue in choosing categories + modify the new categories + fetch sets from firebase (#140)

--- src/components/sets/Sets.jsx
+++ src/components/sets/Sets.jsx
@@ -1,26 +1,15 @@
-import { useState } from 'react';
+/* eslint-disable spaced-comment */
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from 'react';
+import { query, collection, getDocs } from 'firebase/firestore';
+import { db } from '../../services/firebaseConfig';
 import AddSet from './AddSet';
 import SelectSet from './SelectSet';
 
 const Sets = () => {
   const [showForm, setShowForm] = useState(false);
   const [id, setId] = useState(); //Id can be used when creating the cards
-  const [setss, setSets] = useState([
-    {
-      id: 1,
-      name: 'A',
-      image: 'image',
-      description: 'description',
-      category: 'category',
-    },
-    {
-      id: 2,
-      name: 'B',
-      image: 'image2',
-      description: 'description2',
-      category: 'category2',
-    },
-  ]);
+  const [setss, setSets] = useState([]);
 
   const handleFormVisibility = () => {
     setShowForm(!showForm);
@@ -28,6 +17,20 @@ const Sets = () => {
   const onAddingSet = (setObj) => {
     setSets((prevState) => [...prevState, setObj]);
   };
+
+  const fetchSets = async (title) => {
+    const setsRef = collection(db, 'sets');
+    const q = title ? query(setsRef) : query(setsRef);
+    const setsss = await getDocs(q);
+    setsss.docs.forEach((set) => {
+      setSets((prev) => [...prev, { ...set.data() }]);
+    });
+  };
+
+  useEffect(() => {
+    fetchSets();
+  }, [setss]);
+
   return (
     <div>
       {showForm ? (
@@ -41,6 +44,6 @@ const Sets = () => {
       )}
     </div>
   );
-};;
+};
 
 export default Sets;
