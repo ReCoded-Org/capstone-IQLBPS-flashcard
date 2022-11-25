ebb344fc82cecf57d5e8b91d44cd9114507894e9
sara khalil <sarakhalil3377@gmail.com>
2022-11-25 23:13:11 +0300

    ReviewCard Component

--- src/components/SetCard.jsx
+++ src/components/SetCard.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useRef } from 'react';
+import Flippy, { FrontSide, BackSide } from 'react-flippy';
 
 const SetCard = ({ front, back }) => {
+  const ref = useRef();
   return (
     <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-      {front} {back}
+      <Flippy
+        flipOnHover={false}
+        flipOnClick
+        flipDirection="vertical"
+        ref={ref} // to use toggle method like ref.curret.toggle()
+        // if you pass isFlipped prop component will be controlled component.
+        // and other props, which will go to div
+        style={{ width: '200px', height: '200px' }} /// these are optional style, it is not necessary
+      >
+        <FrontSide style={{ backgroundColor: 'white', curser:'pointer' }}>
+          {front} <br />
+          <button
+            onClick={() => {
+              ref.current.toggle();
+            }}
+          />
+        </FrontSide>
+        <BackSide style={{ backgroundColor: 'white' , curser:'pointer'}}>{back}</BackSide>
+      </Flippy>
     </div>
+    //
   );
 };
 
