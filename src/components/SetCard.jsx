baf9d20d2fdecc8ed200841a183e520cbf5d46da
Nijyar Ghazi <nijyar360@gmail.com>
2022-11-28 01:23:09 +0300

    fixed

--- src/components/SetCard.jsx
+++ src/components/SetCard.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import Flippy, { FrontSide, BackSide } from 'react-flippy';
+import ReactHtmlParser from 'react-html-parser';
 
 const SetCard = ({ front, back }) => {
   const ref = useRef();
@@ -11,8 +12,8 @@ const SetCard = ({ front, back }) => {
       ref={ref}
       className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white" /// these are optional style, it is not necessary
     >
-      <FrontSide>
-        {front} <br />
+      <FrontSide className="flex text-center items-center justify-center">
+        {ReactHtmlParser(front)} <br />
         {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
         <button
           type="button"
@@ -21,7 +22,9 @@ const SetCard = ({ front, back }) => {
           }}
         />
       </FrontSide>
-      <BackSide>{back}</BackSide>
+      <BackSide className="flex text-center items-center justify-center">
+        {ReactHtmlParser(back)}
+      </BackSide>
     </Flippy>
   );
 };
